05259917081568aa32309cd68ef405185a94206f
kiehp <kierra.nelson@aol.com>
2021-07-23 23:07:07 -0400

    js functionality

--- passwordgenerator.js
+++ passwordgenerator.js
@@ -1,14 +1,25 @@
 console.log("linked")
 
-//password object array
-function generatePassword() {
-  var length = 12,
-     charset= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-     returnPassword = "";
-//loop through password criteria array
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    returnPassword += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return returnPassword;
+var newPassword = document.querySelector("#generatePassword");
+function getPassword() {
+  var password = generatePassword();
+  var passwordCharacters = document.querySelector("#password");
+
+  passwordCharacters.value = password;
 }
-console.log(generatePassword);
+
+newPassword.addEventListener("click", getPassword);
+
+
+//password object array
+//function generatePassword() {
+ // var length = 12,
+     //charset= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+     //returnPassword = "";
+//loop through password criteria array
+ // for (var i = 0, n = charset.length; i < length; ++i) {
+    //returnPassword += charset.charAt(Math.floor(Math.random() * n));
+ // }
+  //return returnPassword;
+//}
+//console.log(generatePassword);
